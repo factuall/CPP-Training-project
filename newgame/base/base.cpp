574e0b8bb745d2fd59824e2cff006835e2b4dd81
factuall <adrianpolv@gmail.com>
2021-03-24 11:18:52 +0100

    Added base

--- newgame/base/base.cpp
+++ newgame/base/base.cpp
@@ -1,2 +1,29 @@
 #include "Object.h"
 #include "base.h"
+
+const int objLimit = 100;
+Object* Objects[objLimit] = {};
+
+void init() {
+    for (int i = 0; i < objLimit; i++) {
+        Objects[i] = new Object();
+    }
+}
+
+void addObject(Object newObject) {
+    for (int i = 0; i < objLimit; i++) {
+        if (Objects[i]->isNull) {
+            newObject.id = i;
+            *Objects[i] = newObject;
+            return;
+        }
+    }
+}
+
+Object* getObject(int id) {
+    return Objects[id];
+}
+
+int getObjLimit() {
+    return objLimit;
+}
--- newgame/base/base.h
+++ newgame/base/base.h
@@ -1,1 +1,8 @@
 #pragma once
+#include "Object.h"
+extern const int objLimit;
+extern Object* Objects[];
+void init();
+void addObject(Object newObject);
+Object* getObject(int id);
+int getObjLimit();

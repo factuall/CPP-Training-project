167b4750bfd4289b8d464082e44950c4686c4ce7
factuall <adrianpolv@gmail.com>
2021-03-26 08:36:46 +0100

    Cleaned collision detections

--- newgame/base/Collision.cpp
+++ newgame/base/Collision.cpp
@@ -18,6 +18,10 @@ bool Collision::linePoint(int x, int y, int destX, int destY, int pointX, int po
 	return (dist + distDest >= lineLength - buffer && dist + distDest <= lineLength + buffer);
 }
 
+bool Collision::pointCircle(int x, int y, int circleX, int circleY, int radius) {
+	return (distance(x, y, circleX, circleY) <= radius);
+}
+
 bool Collision::areColliding() {
 	if (colliderA->type == Collider::ColliderType::BoxType && colliderB->type == Collider::ColliderType::BoxType) {
 		BoxCollider* A = dynamic_cast<BoxCollider*>(colliderA),
@@ -46,48 +50,30 @@ bool Collision::areColliding() {
 		if (B->y < A->y)         tempY = A->y;      // top edge
 		else if (B->y > A->y + A->height) tempY = A->y + A->height;   // bottom edge
 
-		int distX = B->x - tempX;
-		int distY = B->y - tempY;
-		float distance = sqrt((distX * distX) + (distY * distY));
-		return (distance <= B->radius);
+		//is distance from closest edge smaller than circle radius
+		return (distance(B->x, B->y, tempX, tempY) <= B->radius);
 
 	}
 	if (colliderA->type == Collider::ColliderType::CircleType && colliderB->type == Collider::ColliderType::CircleType) {
 		CircleCollider* A = dynamic_cast<CircleCollider*>(colliderA);
 		CircleCollider* B = dynamic_cast<CircleCollider*>(colliderB);
-		int distX = A->x - B->x;
-		int distY = A->y - B->y;
-		float distance = sqrt((distX * distX) + (distY * distY));
-		return (distance <= (A->radius + B->radius));
+		//is distance between circles smaller than sum of their radius
+		return (distance(A->x, A->y, B->x, B->y) <= (A->radius + B->radius));
 	}
 	if (colliderA->type == Collider::ColliderType::LineType && colliderB->type == Collider::ColliderType::CircleType) {
 		LineCollider* A = dynamic_cast<LineCollider*>(colliderA);
 		CircleCollider* B = dynamic_cast<CircleCollider*>(colliderB);
-		bool inside1 = (sqrt(((A->x - B->x) * (A->x - B->x)) + ((A->y - B->y) * (A->y - B->y))) <= B->radius);
-		bool inside2 = (sqrt(((A->destX - B->x) * (A->destX - B->x)) + ((A->destY - B->y) * (A->destY - B->y))) <= B->radius);
-		if (inside1 || inside2) return true;
-		int distX = A->x - A->destX;
-		int distY = A->y - A->destY;
-		float len = sqrt((distX * distX) + (distY * distY));
-		float dot = (((B->x - A->x) * (A->destX - A->x)) + ((B->y - A->y) * (A->destY - A->y))) / pow(len, 2);
+		//line start or destination in circle
+		if (pointCircle(A->x, A->y, B->x, B->y, B->radius) ||
+			pointCircle(A->destX, A->destY, B->x, B->y, B->radius)) return true;
+		float lineLength = distance(A->x, A->y, A->destX, A->destY);
+		float dot = (((B->x - A->x) * (A->destX - A->x)) + ((B->y - A->y) * (A->destY - A->y))) / pow(lineLength, 2);
 		float closestX = A->x + (dot * (A->destX - A->x));
 		float closestY = A->y + (dot * (A->destY - A->y));
-		float dstX1 = closestX - A->x;
-		float dstY1 = closestY - A->y;
-		float dstX2 = closestX - A->destX;
-		float dstY2 = closestY - A->destY;
-		float d1 = sqrt((dstX1 * dstX1) + (dstY1 * dstY1));
-		float d2 = sqrt((dstX2 * dstX2) + (dstY2 * dstY2));
-		float buffer = 0.1; 
-			if (!(d1 + d2 >= len - buffer && d1 + d2 <= len + buffer)) {
-				return false;
-			}
-		float distX = closestX - B->x;
-		float distY = closestY - B->y;
-		float distance = sqrt((distX * distX) + (distY * distY));
-		if (distance <= B->radius) {
-			return true;
-		}
+		//is closest point on the line
+		if (!linePoint(A->x, A->y, A->destX, A->destY, closestX, closestY)) return false;
+		//if closest point in circle
+		if (distance(closestX, closestY, B->x, B->y) <= B->radius) return true;
 	}
 	return false;				
 };
--- newgame/base/Collision.h
+++ newgame/base/Collision.h
@@ -6,6 +6,7 @@ public:
 	Collider* colliderB;
 	bool areColliding();
 private:
-	bool linePoint(int x, int y, int destX, int destY, int pointX, int pointY);
 	float distance(int x, int y, int destX, int destY);
+	bool pointCircle(int x, int y, int circleX, int circleY, int radius);
+	bool linePoint(int x, int y, int destX, int destY, int pointX, int pointY);
 };
